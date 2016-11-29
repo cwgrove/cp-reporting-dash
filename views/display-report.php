cc6c967abca8b2d166af6330d51e0fe4c45d3c3b
Christian <ccampbell@sapphirebd.com>
2016-11-29 15:09:32 -0700

    fiblars
    wiz

--- views/display-report.php
+++ views/display-report.php
@@ -1,5 +1,13 @@
 <?php
 
+//$student->last_login;
+
+
+
+
+
+
+
 
 
 
@@ -43,12 +51,43 @@ $f = $i+1;
 
     <?php
     foreach ($students as $key => $student) {
+/// calc enrollment time
+      $enroll_date = strtotime($student->enrolled_course_date);
+      $current_time = time();
+      $time_diff = $current_time - $enroll_date;
+      //seconds
+      $time_diff = $time_diff/60;
+      $time_diff = $time_diff/60;
+      $time_diff = $time_diff/24;
+      $time_diff = round($time_diff);
+
+      if ($student->course_progress < 1) {
+        $stnt =  "Has not started since ".$time_diff." Days ago";
+      }else {
+     $stnt =  "Started ".$time_diff." Days ago";
+      }
+
+///calc dasys since logged in
+
+  $ll = $student->last_login ;
+  $lld = $current_time - $ll;
+  $lld = $lld/60;
+  $lld = $lld/60;
+  $lld = round($lld/24);
+  if($lld < 1){
+  $ll_stanement = "Today";
+  }
+  $ll_stanement = $lld." Days ago";
+
+
+
       echo "<tr>";
         echo "<td>".$student->name."</td>";
         echo "<td>".$student->email."</td>";
         echo "<td>".$student->course_progress."</td>";
-        echo "<td>".$student->enrolled_course_date."</td>";
-        echo "<td>".$student->last_login."</td>";
+        //echo "<td>".$student->enrolled_course_date."</td>";
+        echo "<td>".$stnt."</td>";
+        echo "<td>".$ll_stanement."</td>";
 
         for ($i=0; $i <$number_of_units ; $i++) {
           $f = $i+1;
